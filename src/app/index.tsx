abcf9482ae8152291f46dc9e5c655553f42d5cd7
EclecticForge <eclecticforge@gmail.com>
2026-09-19 20:29:43 +0200

    ADD Grid

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,36 +1,74 @@
 import { useAudioPlayer } from "expo-audio";
-import { StyleSheet, Text, View } from "react-native";
 import AssetManager from "../app/assetmanager";
+import '../app/styles.css';
 
-function test(assets:AssetManager) {
+function test(assets: AssetManager) {
 
+  let number1: number = Math.floor(Math.random()*7+1);
+  let number2: number = Math.floor(Math.random()*7+1);
+  return (
+  <div className="item">
+    <img src={assets[0][7][number1]} className="cell"/>
+    <img src={assets[0][0][number2]} className="cell"/>
+  </div> 
+  );
 }
+  
 
 export default function Index() {
 
   const assets: AssetManager = new AssetManager();
-  
+
   const backgroundplayer = useAudioPlayer(assets[1][0]);
   backgroundplayer.loop = true;
   backgroundplayer.volume = 0.85;
   backgroundplayer.play();
 
-  const effectplayer = useAudioPlayer(assets[1][1]);
-  effectplayer.loop = true;
+  const effectplayer = useAudioPlayer();
   effectplayer.volume = 1;
-  effectplayer.play();
+
 
   return (
-    <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-    </View>
+    <div className = "page">
+      <div className = "grider">
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        {test(assets)}
+        </div>
+    </div>
+
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
